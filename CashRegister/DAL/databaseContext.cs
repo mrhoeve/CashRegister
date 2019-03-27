85c8c1eec602569587413806fac04b16e4c8a60c
Marc Hoeve <mrhoeve@gmail.com>
2019-03-28 00:27:38 +0100

    Rename databaseContext

--- CashRegister/DAL/databaseContext.cs
+++ CashRegister/DAL/databaseContext.cs
@@ -1,10 +1,7 @@
 ﻿using CashRegister.DataModels;
 using System;
-using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CashRegister.DAL
 {
