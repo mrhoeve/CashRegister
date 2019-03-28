931ef56495a81ac90a64b3e787e2ff0457b892db
Marc Hoeve <mrhoeve@gmail.com>
2019-03-28 21:29:19 +0100

    Added tests for CurUser timeout testing

--- UnitTests/Model/CurUserTest.cs
+++ UnitTests/Model/CurUserTest.cs
@@ -2,7 +2,10 @@
 using CashRegister.DataModels;
 using CashRegister.Model;
 using NUnit.Framework;
+using System;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using UnitTests.DAL;
 
 namespace UnitTests.Model
@@ -29,6 +32,49 @@ namespace UnitTests.Model
         }
 
         [Test]
+        public void test_isLoggedIn_NewInstanceWithRightCredentials_WaitForTimeOut_FinallyExpectFalse()
+        {
+            // Setup test
+            CurUser curUser = CurUser.curUser;
+            object o = curUser;
+            var t = typeof(CurUser);
+            // Change the private (!!) property from the initial 5 minutes to 2 seconds
+            t.GetProperty("_timerInterval", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(o, TimeSpan.FromSeconds(2));
+            // Log in with the right credentials
+            curUser.LogIn(Definitions.localAdminP, Definitions.TEST_PASSWORD_VALID);
+            // Are we logged in?
+            Assert.IsTrue(curUser.isLoggedIn());
+            // Now wait for the adjusted timer to expire
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+            // And check again if we're logged in
+            Assert.IsFalse(curUser.isLoggedIn());
+        }
+
+        [Test]
+        public void test_isLoggedIn_NewInstanceWithRightCredentials_DelayForTimeOut_FinallyExpectFalse()
+        {
+            // Setup test
+            CurUser curUser = CurUser.curUser;
+            object o = curUser;
+            var t = typeof(CurUser);
+            // Change the private (!!) property from the initial 5 minutes to 3 seconds
+            t.GetProperty("_timerInterval", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(o, TimeSpan.FromSeconds(3));
+            // Log in with the right credentials
+            curUser.LogIn(Definitions.localAdminP, Definitions.TEST_PASSWORD_VALID);
+            for (int i = 0; i < 10; i++)
+            {
+                // Now wait for the adjusted timer to nearly expire
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                // Are we logged in? -- This should reset the timer
+                Assert.IsTrue(curUser.isLoggedIn());
+            }
+            // Now wait for the adjusted timer to expire
+            Thread.Sleep(TimeSpan.FromSeconds(4));
+            // And check again if we're logged in
+            Assert.IsFalse(curUser.isLoggedIn());
+        }
+
+        [Test]
         public void test_isLoggedIn_NewInstanceWithoutRightCredentials_ExpectFalse()
         {
             CurUser curUser = CurUser.curUser;
@@ -47,7 +93,7 @@ namespace UnitTests.Model
         public void test()
         {
             Persoon sg = Context.getInstance().Get().Persoon.Where(p => p.Id == Definitions.localAdminP.Id).SingleOrDefault();
-            Assert.IsTrue(sg!=null);
+            Assert.IsTrue(sg != null);
         }
     }
 }
